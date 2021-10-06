552fa59d641d5a3e654dffd4aa9809c1f3d5faca
Junjie Yang <jjyusa2010@gmail.com>
2021-10-05 21:49:33 -0700

    replace over push

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ export default function Custom404() {
   const router = useRouter();
   useEffect(() => {
     setTimeout(() => {
-      router.push("/");
+      router.replace("/");
     }, 2000);
   }, []);
 
